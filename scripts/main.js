2c66d1e4619bc9aa378c1d100e7455621d872854
Kate Gordon <kategordon.code@gmail.com>
2019-09-24 11:54:14 -0400

    inital

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const chuckQuotesForm = document.querySelector("#chuckQuotesForm");
+
+chuckQuotesForm.addEventListener("submit", function(event){
+    event.preventDefault(); 
+    const categoryValue = chuckQuotesForm.querySelector("select").value; 
+    updateChuckSays(categoryValue); 
+})
+
+
+function updateChuckSays(category) {
+    const chuckSays = document.getElementById("chuckSays");
+    get(`https://api.chucknorris.io/jokes/random?category=${category}`).then( 
+    response => {
+    chuckSays.innerHTML = response.value;   
+})
+}
+function getCategories() {
+    const selectWrapper = document.querySelector("#selectWrapper");
+    const categoryList = document.createElement("select"); 
+
+    get('https://api.chucknorris.io/jokes/categories').then(function(response){
+        response.forEach(function(category){
+            const categoryOption = document.createElement("option"); 
+            categoryOption.text = category;
+            categoryOption.value = category; 
+            categoryList.append(categoryOption); 
+        }); 
+    });
+    selectWrapper.append(categoryList); 
+}
+
+// Create an IMMEDIATELY INVOKED FUNCTION EXPRESSION (IIFE) - anonymous functions that run immediately
+(function(){
+    const defaultCategory = "dev";
+    getCategories();
+    updateChuckSays(defaultCategory); 
+})();
+
+
+
+
+
+
